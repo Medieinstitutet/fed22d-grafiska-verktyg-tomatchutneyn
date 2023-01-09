023b37b3c1f7ebdc5556d07bf28797307f0f81f5
Grapensparr <113124538+Grapensparr@users.noreply.github.com>
2023-01-09 17:29:06 +0100

    Starting point

---
+++ script.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector('.menu');
+const startingPage = document.querySelector('.startingPage');
+const prices = document.querySelector('.prices');
+const aboutUs = document.querySelector('.aboutUs');
+const footer = document.querySelector('.footer');
+
+menu.innerHTML = 
+    '<h4>Startsida</h4>' +
+    '<h4>Fina priser</h4>' +
+    '<h4>Om oss</h4>';

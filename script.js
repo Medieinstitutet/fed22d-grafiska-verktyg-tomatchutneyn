99d66d582198a5ddfb0459074c59b891201e9a02
Grapensparr <113124538+Grapensparr@users.noreply.github.com>
2023-01-10 11:44:50 +0100

    Starting page

--- script.js
+++ script.js
@@ -2,9 +2,40 @@ const menu = document.querySelector('.menu');
 const startingPage = document.querySelector('.startingPage');
 const prices = document.querySelector('.prices');
 const aboutUs = document.querySelector('.aboutUs');
-const footer = document.querySelector('.footer');
+const cookieBar = document.querySelector('.cookieBar');
+const inputField = document.createElement('input');
+const registerBtn = document.createElement('button');
+const cookieBtn = document.createElement('button');
+const overlayColor = document.createElement('div');
+const body = document.body;
+const popup = document.createElement('div');
 
 menu.innerHTML = 
-    '<h4>Startsida</h4>' +
-    '<h4>Fina priser</h4>' +
-    '<h4>Om oss</h4>';
+    '<h4><a href="#">Startsida</a></h4>' +
+    '<h4><a href="#">Fina priser</a></h4>' +
+    '<h4><a href="#">Om oss</a></h4>';
+
+startingPage.innerHTML +=
+    '<h4>Lördagen den 30 oktober blir det maskerad i Ljushallen, Stora gatan 29, Stockholm. Räkna med en skrämmande upplevelse. Personen med läskigast utklädnad vinner pris! <br>' + 
+    'O s a senast 22 oktober via anmälan nedan.</h4><br>';
+
+inputField.placeholder = 'Mailadress här:';
+startingPage.appendChild(inputField);
+
+registerBtn.innerHTML = 'Anmäl dig här!';
+startingPage.appendChild(registerBtn);
+
+cookieBar.innerHTML = '<p>Acceptera cookies</p>';
+cookieBtn.innerHTML = 'Godkänn';
+cookieBar.appendChild(cookieBtn);
+
+cookieBtn.addEventListener('click', () => {
+    cookieBar.remove();
+});
+
+registerBtn.addEventListener('click', () => {
+    overlayColor.classList.add('overlayColor');
+    popup.classList.add('popup');
+    body.append(overlayColor);
+    overlayColor.append(popup);
+});

7cd2adcca491e5b688e3b409b550ff94c110abab
Veronica <veronica.bergstrom@medieinstitutet.se>
2023-01-10 13:52:42 +0100

    prettier config

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ const overlayColor = document.createElement('div');
 const body = document.body;
 const popup = document.createElement('div');
 
-menu.innerHTML = 
-    '<h4><a href="#">Startsida</a></h4>' +
-    '<h4><a href="#">Fina priser</a></h4>' +
-    '<h4><a href="#">Om oss</a></h4>';
+menu.innerHTML =
+  '<h4><a href="#">Startsida</a></h4>' +
+  '<h4><a href="#">Fina priser</a></h4>' +
+  '<h4><a href="#">Om oss</a></h4>';
 
 startingPage.innerHTML +=
-    '<h4>Lördagen den 30 oktober blir det maskerad i Ljushallen, Stora gatan 29, Stockholm. Räkna med en skrämmande upplevelse. Personen med läskigast utklädnad vinner pris! <br>' + 
-    'O s a senast 22 oktober via anmälan nedan.</h4><br>';
+  '<h4>Lördagen den 30 oktober blir det maskerad i Ljushallen, Stora gatan 29, Stockholm. Räkna med en skrämmande upplevelse. Personen med läskigast utklädnad vinner pris! <br>' +
+  'O s a senast 22 oktober via anmälan nedan.</h4><br>';
 
 inputField.placeholder = 'Mailadress här:';
 startingPage.appendChild(inputField);
@@ -30,12 +30,12 @@ cookieBtn.innerHTML = 'Godkänn';
 cookieBar.appendChild(cookieBtn);
 
 cookieBtn.addEventListener('click', () => {
-    cookieBar.remove();
+  cookieBar.remove();
 });
 
 registerBtn.addEventListener('click', () => {
-    overlayColor.classList.add('overlayColor');
-    popup.classList.add('popup');
-    body.append(overlayColor);
-    overlayColor.append(popup);
+  overlayColor.classList.add('overlayColor');
+  popup.classList.add('popup');
+  body.append(overlayColor);
+  overlayColor.append(popup);
 });
